b348f0b2b4644d7ba79be04d3584c29e4e4c8a33
Trevor D. Miller <trevordmiller@icloud.com>
2016-07-23 18:14:09 -0600

    Add test for lowercase slug conversion

--- src/getURLSlug.test.js
+++ src/getURLSlug.test.js
@@ -2,5 +2,9 @@ import test from 'ava';
 import getURLSlug from './getURLSlug';
 
 test('URL slugs are created from strings', t => {
-  t.is(getURLSlug('Hello World'), 'hello-world');
+  t.is(getURLSlug('hello world'), 'hello-world');
+});
+
+test('Uppercase letters are converted to lower case', t => {
+  t.is(getURLSlug('HeLLo WorLD'), 'hello-world');
 });
